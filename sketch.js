32ab64f89c3620706d9a4100031839eba2780ef5
Sam <63341022+whereissam@users.noreply.github.com>
2021-06-18 17:34:34 +0800

    Pose estimation with PoseNet

--- sketch.js
+++ sketch.js
@@ -1,21 +1,51 @@
-let soundClassifier;
-let resultP
-function preload(){
-  let options = { probabilityThreshold: 0.95 } 
-  //tirgger machine to do sound clasify
-  soundClassifier = ml5.soundClassifier('SpeechCommands18w', options)
-}
+let video
+let poseNet
+let pose
+let skeleton
 
 function setup() {
-  createCanvas(400, 400)
-  resultP = createP('waiting...')
-  resultP.style('font-size', '32pt')
-  soundClassifier.classify(gotResults)
+  createCanvas(640, 480)
+  video = createCapture(VIDEO)
+  video.hide()
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses)
 }
 
-function gotResults(error, results){
-  if(error){
-    console.log(error)
+function gotPoses(poses){
+  console.log(poses)
+  if(poses.length > 0){
+    pose = poses[0].pose
+    skeleton = poses[0].skeleton
   }
-  resultP.html(`${results[0].label} : ${results[0].confidence}`)
+}
+
+function modelLoaded(){
+  console.log('poseNet ready')
+}
+
+function draw() {
+  image(video, 0,0)
+
+  if(pose){
+    fill(255,0,0)
+    ellipse(pose.nose.x, pose.nose.y, 100)
+    fill(0,0,255)
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32)
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32)
+
+    for(let i=0;i< pose.keypoints.length; i++){
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0,255,0)
+      ellipse(x,y,16,16)
+    }
+
+    for(let i=0; i< skeleton.length; i++){
+      let a = skeleton[i][0]
+      let b = skeleton[i][1]
+      strokeWeight(2)
+      stroke(255)
+      line(a.position.x, a.position.y, b.position.x, b.position.y)
+    }
+  }
 }
